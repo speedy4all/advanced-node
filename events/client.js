182eb871a3df83329695b7c217e664637511f84a
Bogdan Ion BUESCU <bbuescu@PENTALOG.com>
2018-10-08 17:19:46 +0300

    event emitter tasks app

---
+++ events/client.js
@@ -0,0 +1,21 @@
+const EventEmitter = require('events');
+const readLine = require('readline');
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client = new EventEmitter();
+const server = require('./server')(client);
+server.on('response', (response) => {
+    process.stdout.write('\u001b[2J\u001b[0;0f');
+    process.stdout.write(response);
+    process.stdout.write('\n\>');
+});
+
+let command, args;
+rl.on('line', (input) => {
+    [command, ...args] = input.split(' ');
+    client.emit('command', command, args);
+});
